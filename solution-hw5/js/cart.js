a4ac3a041e652699824991232b190e3e810fd935
zaynabattaras <143553207+zaynabattaras@users.noreply.github.com>
2023-10-07 23:13:54 -0400

    final version of hw5

--- solution-hw5/js/cart.js
+++ solution-hw5/js/cart.js
@@ -1,9 +1,9 @@
 //making a dictionary with glazing info
 let glazing = {
-    "Original":0, 
-    "Sugar Milk": 0,
-    "Vanilla Milk": 0.5,
-    "Double Chocolate": 1.50,
+    "Keep original":0, 
+    "Sugar milk": 0,
+    "Vanilla milk": 0.5,
+    "Double chocolate": 1.50,
 
 };
 //making a dictionary with packing size info
@@ -13,16 +13,7 @@ let packing = {
     "6":5,
     "12":10,
 };
-let cart = [];
-class Roll {
-    constructor(rollType, rollGlazing, packSize, rollPrice, totalPrice) {
-        this.type = rollType;
-        this.glazing =  rollGlazing;
-        this.size = packSize;
-        this.basePrice = rollPrice;
-        this.totalPrice = totalPrice;
-    }
-}
+// calculates total price of a roll based on glazing, packsize, baseprice
 function totalPriceCalc (glazingV, packSize, basePrice) { 
     let tempPrice = (basePrice + glazing[glazingV]) * packing[packSize];
     console.log(glazing[glazingV]);
@@ -30,50 +21,91 @@ function totalPriceCalc (glazingV, packSize, basePrice) {
     //https://www.stechies.com/javascript-round-two-decimal-places/
     return tempPrice.toFixed(2); 
 }
+let cart = new Set();
+// class that stores roll attributes
+class Roll {
+    constructor(rollType, rollGlazing, packSize, rollPrice) {
+        this.type = rollType;
+        this.glazing =  rollGlazing;
+        this.size = packSize;
+        this.basePrice = rollPrice;
+        this.element = null;
+        this.rollCalcPrice = totalPriceCalc(this.glazing, this.size, this.basePrice);
+    }
+}
 
-let originalRoll = new Roll("Original","Sugar Milk", "1", 2.49, totalPriceCalc("Sugar Milk", "1", 2.49));
-let walnutRoll = new Roll("Walnut","Vanilla Milk", "12", 3.49, totalPriceCalc("Vanilla Milk", "12", 3.49));
-let raisinRoll = new Roll("Raisin","Sugar Milk", "3",2.99, totalPriceCalc("Sugar Milk", "3",2.99));
-let appleRoll = new Roll("Apple", "Original","3",3.49, totalPriceCalc("Original","3",3.49));
-cart.push(originalRoll);
-cart.push(walnutRoll);
-cart.push(raisinRoll);
-cart.push(appleRoll);
+// creating instances for roll entries
+let originalRoll = new Roll("Original","Sugar milk", "1", 2.49);
+let walnutRoll = new Roll("Walnut","Vanilla milk", "12", 3.49);
+let raisinRoll = new Roll("Raisin","Sugar milk", "3",2.99);
+let appleRoll = new Roll("Apple", "Keep original","3",3.49);
+// adding all the rolls to cart set
+cart.add(originalRoll);
+cart.add(walnutRoll);
+cart.add(raisinRoll);
+cart.add(appleRoll);
 console.log(cart);
 
-let templateImg = document.getElementById("images");
-let col1 = document.getElementsByClassName("sc-container1-cols")[0];
-//let clone = templateImg.content.cloneNode(true);
-let shadowImg = col1.attachShadow({ mode: "open" });
 
-let templateDetails = document.getElementById("productDetails");
-let col2= document.getElementById("c1col2");
+//roll is an instance of rolls class
+let rollsTotalPrice = document.querySelector("#total-price");
+let totalPrice = 0;
 
-let shadowDetail = col2.attachShadow({ mode: "open" });
+//function that adds roll elements to the DOM
+function addingRolls(roll){
 
+    let template = document.getElementById("template");
+    let clone = template.content.cloneNode(true);
+    roll.element = clone.querySelector(".roll");// clone  roll layout
 
-function addingRolls(rollI){
-    let rollInfo = rolls[rollI.type];
+    
+    let remove = roll.element.querySelector('#remove');
+    // when remove is clikced will remove DOM elements for that roll
+    remove.addEventListener('click', () => {
+        deleteRoll(roll);
+      });
 
-    let cloneTempImg = templateImg.content.cloneNode(true); 
-    imageTemp = cloneTempImg.querySelector(".sc");
-    imageTemp.src = "..//assets/products/" + rollInfo["imageFile"];
-    imageTemp.alt =rollI.type + " Cinnamon Roll"
-    shadowImg.appendChild(cloneTempImg);
+    let rollElementList = document.querySelector("#roll-list");
+    rollElementList.append(roll.element);
 
-    let cloneTempDetail = templateDetails.content.cloneNode(true); 
-    let detailTemp = cloneTempDetail.querySelector(".sc-container1-col2-rows");
-    detailTemp.innerHTML = '<p class = sc-container1-col2-rows>' + imageTemp.alt + '<br> Glazing: ' +  rollI.glazing + '<br> Pack Size: ' + rollI.size + '</p>';
-    shadowDetail.append(cloneTempDetail);
+    addRoll(roll);
   }
-  for (let i = 0; i < cart.length; i++) {
-    addingRolls(cart[i]);
+  // add roll information to the DOM
+  function addRoll(roll){
+
+    let imageTemp = roll.element.querySelector("#image");
+    let detailTemp = roll.element.querySelector("#details");
+    let priceTemp = roll.element.querySelector("#roll-price");
+    let rollInfo = rolls[roll.type];
+
+
+    imageTemp.src = "../assets/products/" + rollInfo["imageFile"];
+    imageTemp.alt =roll.type + " Cinnamon Roll";
+
+    detailTemp.innerHTML = imageTemp.alt + '<br> <br>' +  roll.glazing + '<br> <br> Pack Size: ' + roll.size ;
+
+    priceTemp.innerText = "$ " +  roll.rollCalcPrice;
+    
+
   }
+  // deletes DOM elements when roll removed and deletes it from cart
+  function deleteRoll(roll){
+    console.log("in delete roll");
+    roll.element.remove();
+    cart.delete(roll);
+    totalPrice = totalPrice - Number(roll.rollCalcPrice);
+    //how to use abs in javascript
+    //https://www.w3schools.com/jsref/jsref_abs.asp
+    let stringfyTotalPrice = Math.abs(totalPrice).toFixed(2);
+    rollsTotalPrice.innerText = "$ " +  stringfyTotalPrice;
+
+  }
+  //adds all rolls in cart set
+  for (const roll of cart) {
+    addingRolls(roll);
+    totalPrice += Number(roll.rollCalcPrice);
+  }
+  // updates DOM with new total price
+  rollsTotalPrice.innerText = "$ " +  totalPrice;
   
-//let clone2 = template.content.cloneNode(true);
-//let clone3 = template.content.cloneNode(true);
-//shadow.appendChild(clone);
-//shadow.appendChild(clone2);
-//shadow.appendChild(clone3);
 
-//document.body.appendChild(clone);
